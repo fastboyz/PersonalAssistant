71c7f6918d6dfff08786f368da47e6e82c039490
Steve Mbiele <stevembiele@gmail.com>
2021-01-05 20:29:57 -0500

    added the feature extraction scripts

--- FeatureExtraction/audio_processor.py
+++ FeatureExtraction/audio_processor.py
@@ -7,13 +7,6 @@ import os
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-# testFile = "E:\\Project\\en_dataset\\Background\\noise.wav"
-# features = {
-#     "mapping": [],
-#     "mfcc": [],
-#     "labels": []
-# }
-
 """
 Main Issue: all recording don't have the same length 
 Sam File ->5 sec  but in general 2-3 sec 
@@ -40,10 +33,6 @@ def split_audio_file(file, seconds, save_path):
 
 
 def dir_mode(dir, seconds, save_path):
-    # Walk the dir
-    # Get all the files
-    # Split each files
-    # save them into the folder
     for i, (dirpath, dirname, filenames) in enumerate(os.walk(dir)):
         for file in filenames:
             split_audio_file(os.path.join(dirpath, file), seconds=seconds, save_path=save_path)
@@ -68,52 +57,3 @@ if __name__ == '__main__':
         dir_mode(args.dir, args.seconds, args.save_path)
     elif args.file is None and args.dir is None:
         raise Exception("Please set either the --file or --dir to run the Script")
-
-    # signal, sr = librosa.load(testFile, sr=8000)
-    #
-    # librosa.display.waveplot(signal, sr=sr)
-    # plt.xlabel("Time")
-    # plt.ylabel("Amplitude")
-    # plt.title("waveform")
-    # plt.show()
-    #
-    # ## Fast Fourier Transform (FFT) -> Spectrum
-    #
-    # fft = np.fft.fft(signal)
-    # magnitude = np.abs(fft)
-    # frequency = np.linspace(0, sr, len(magnitude))
-    #
-    # left_magnitude = magnitude[:int(len(magnitude) / 2)]
-    # left_frequency = frequency[:int(len(frequency) / 2)]
-    #
-    # plt.plot(left_frequency, left_magnitude)
-    # plt.xlabel("Frequency")
-    # plt.ylabel("Magnitude")
-    # plt.title("Spectrum")
-    # plt.show()
-    #
-    # ## Short Time Fourier Transform (STFT) -> Spectrogram
-    # # Number of samples per ftt
-    # n_fft = 2048
-    # hop_length = 512
-    #
-    # stft = librosa.core.stft(signal, n_fft=n_fft, hop_length=hop_length)
-    # spectrogram = np.abs(stft)
-    #
-    # log_spectrogram = librosa.amplitude_to_db(spectrogram)
-    #
-    # librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
-    # plt.xlabel("Time")
-    # plt.ylabel("Frequency")
-    # plt.title("Spectrogram")
-    # plt.show()
-    #
-    # ## MFCCs
-    # mfccs = librosa.feature.mfcc(signal, n_fft=n_fft, hop_length=hop_length, n_mfcc=13)
-    # librosa.display.specshow(mfccs, sr=sr, hop_length=hop_length)
-    # plt.xlabel("Time")
-    # plt.ylabel("MFCC")
-    # plt.title("MFCCs")
-    # plt.show()
-
-    # print(testFile)
---
+++ FeatureExtraction/feature_extraction.py
@@ -0,0 +1,61 @@
+import argparse
+import json
+import math
+import os
+
+import librosa
+
+features = {
+    "mapping": [],
+    "mfcc": [],
+    "labels": []
+}
+
+
+def get_mfccs(dataset_path, json_path, n_mfcc, n_fft, hop_length, expected_num_mfccs_vectors, sample_rate):
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+        if dirpath is not dataset_path:
+            semantic_label = dirpath.split(os.path.sep)[-1]
+            features["mapping"].append(semantic_label)
+            print("Processing: {}".format(semantic_label))
+
+            for f in filenames:
+                # Load the file
+                print(f)
+                file_path = os.path.join(dirpath, f)
+                signal, sr = librosa.load(file_path, sr=sample_rate)
+
+                mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+                mfcc = mfcc.T
+
+                if len(mfcc) == expected_num_mfccs_vectors:
+                    features["mfcc"].append(mfcc.tolist())
+                    features["labels"].append(i-1)
+
+    with open(json_path, 'w') as fp:
+        json.dump(features, fp, indent=2)
+
+
+def main(args):
+    sample_per_track = args.duration * args.sample_rate
+    expected_num_mfccs_vectors = math.ceil(sample_per_track / args.hop_length)
+    get_mfccs(dataset_path=args.dataset_path, json_path=args.json_path, n_mfcc=args.n_mfcc, hop_length=args.hop_length,
+              n_fft=args.n_fft, expected_num_mfccs_vectors=expected_num_mfccs_vectors, sample_rate=args.sample_rate)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="""
+        This Script will split any audio file passed in parameter into chunks of a specified length
+        """)
+
+    parser.add_argument("--dataset_path", type=str, help="This is the path to the directory where the datasets reside",
+                        required=True)
+    parser.add_argument("--json_path", type=str, help="This is the path to the Resulting json folder", required=True)
+    parser.add_argument("--duration", type=int, default=3, help="Length the audio files")
+    parser.add_argument("--sample_rate", type=int, default=8000, help="The Audio files sample rate")
+    parser.add_argument("--hop_length", type=int, default=512, help="The Hop length")
+    parser.add_argument("--n_mfcc", type=int, default=13, help="Number of MFCCs")
+    parser.add_argument("--n_fft", type=int, default=2048, help="Number of FTTs")
+
+    args = parser.parse_args()
+    main(args)
